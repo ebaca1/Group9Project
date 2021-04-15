7a8fcdb57e1a6f8ba2556c38973cd8e1ef8ab5af
Sarah Dieffenbaugher <skoehli@uncc.edu>
2021-04-15 13:23:30 -0400

    Added database file - Sarah Dieffenbaugher

---
+++ event_app/database.py
@@ -0,0 +1,12 @@
+# import flask-sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize new instance of Flask-SQLAlchemy extension
+db = SQLAlchemy()
+
+# Import database instance
+from database import db
+
+
+
+
