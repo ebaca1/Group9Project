27df13197370b99da24ed457c0c90551852c0292
Katie Anders <77994465+kanders2002@users.noreply.github.com>
2021-04-15 15:05:27 -0400

    Update event_app.py

--- event_app/event_app.py
+++ event_app/event_app.py
@@ -1,1 +1,10 @@
-# imports
+import os 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+from flask import render_template, request, redirect, url_for, session
+from database import db
+from models import Note as Note, User as User, Comment as Comment
+from forms import RegisterForm, LoginForm, RegisterForm, LoginForm, CommentForm
+import bcrypt
