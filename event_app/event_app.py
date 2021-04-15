f14ed8ea7d042e76dfe711ffa6175b8bacfded85
Katie Anders <77994465+kanders2002@users.noreply.github.com>
2021-04-15 15:15:03 -0400

    Update event_app.py

--- event_app/event_app.py
+++ event_app/event_app.py
@@ -8,3 +8,54 @@ from database import db
 from models import Note as Note, User as User, Comment as Comment
 from forms import RegisterForm, LoginForm, RegisterForm, LoginForm, CommentForm
 import bcrypt
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY'] = 'SE3155'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+@app.route('/login')
+def login():
+
+    return render_template("login.html")
+  
+@app.route('/')
+@app.route('/login')
+def login():
+#insert code
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+#insert code
+    return render_template("register.html")
+
+@app.route('/index')
+def index():
+#insert code
+    return render_template("index.html")
+
+@app.route('/new')
+def new():
+#insert code
+    return render_template("new.html")
+
+@app.route('/edit/eventID')
+def edit():
+#insert code
+    return render_template("edit.html")
+
+@app.route('/index/eventID')
+def event():
+#insert code
+    return render_template("event.html")
+
+@app.route('/profile/userID')
+def profile():
+#insert code
+    return render_template("profile.html")
