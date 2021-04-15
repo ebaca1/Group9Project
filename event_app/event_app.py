836a3a4b2a90b57a26cbc45186f19ed7b06e8b61
Katie Anders <77994465+kanders2002@users.noreply.github.com>
2021-04-15 15:19:54 -0400

    Update event_app.py

--- event_app/event_app.py
+++ event_app/event_app.py
@@ -1,3 +1,4 @@
+# IMPORTS #
 import os 
 from flask import Flask, render_template
 
@@ -9,6 +10,8 @@ from models import Note as Note, User as User, Comment as Comment
 from forms import RegisterForm, LoginForm, RegisterForm, LoginForm, CommentForm
 import bcrypt
 
+# SETUP #
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SECRET_KEY'] = 'SE3155'
@@ -18,43 +21,44 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+# LOGIN PAGE #  
 @app.route('/')
 @app.route('/login')
 def login():
 
     return render_template("login.html")
   
-@app.route('/')
-@app.route('/login')
-def login():
-#insert code
-    return render_template("login.html")
-
+# REGISTRATION PAGE #
 @app.route('/register')
 def register():
 #insert code
     return render_template("register.html")
 
+# EVENTS (HOME) PAGE #
 @app.route('/index')
 def index():
 #insert code
     return render_template("index.html")
 
+# CREATE PAGE #
 @app.route('/new')
 def new():
 #insert code
     return render_template("new.html")
 
+# INDIVIDUAL EVENT (EDIT) PAGE #
 @app.route('/edit/eventID')
 def edit():
 #insert code
     return render_template("edit.html")
 
+# INDIVIDUAL EVENT PAGE #
 @app.route('/index/eventID')
 def event():
 #insert code
     return render_template("event.html")
 
+# ACCOUNT INFO PAGE #
 @app.route('/profile/userID')
 def profile():
 #insert code
