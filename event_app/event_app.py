a74ccb1b3b7ba6149d5e34cbb8f118b69d6918bf
aarnett1 <78062193+aarnett1@users.noreply.github.com>
2021-04-16 21:25:45 -0400

    added code to edit function, moved imports to top, fixed minor error in new function - Alison

--- event_app/event_app.py
+++ event_app/event_app.py
@@ -1,14 +1,12 @@
 # IMPORTS #
 import os 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-from flask import render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from database import db
 from models import Event as Event, User as User, Rsvp as Rsvp
 from forms import RegisterForm, LoginForm
 import bcrypt
+
+app = Flask(__name__)
 
 # SETUP #
 #Pulled this directly from flask_app tutorial. Edit/delete what needs to be fixed
@@ -48,7 +46,7 @@ def new():
             title = request.form['title']
             text = request.form['noteText']
             date = request.form['date']
-            new_record = Note(title, text, date, session['user_id'])
+            new_record = Event(title, text, date, session['user_id'])
             db.session.add(new_record)
             db.session.commit()
 
@@ -60,9 +58,24 @@ def new():
 
 # INDIVIDUAL EVENT (EDIT) PAGE #
 @app.route('/edit/eventID')
-def edit():
-#insert code
-    return render_template("edit.html")
+def edit(id, email):
+    if request.method == 'POST':
+        title = request.form['title']
+        date = request.form['date']
+        desc = request.form['desc']
+        event = db.session.query(Event).filter_by(id=id).one()
+        event.title = title
+        event.date = date
+        event.desc = desc
+        db.session.add(event)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:
+        a_user = db.session.query(User).filter_by(email=email).one()
+        my_note = db.session.query(Event).filter_by(id=id).one()
+
+        return render_template("edit.html", note=my_note, user=a_user)
 
 # INDIVIDUAL EVENT PAGE #
 @app.route('/index/eventID')
